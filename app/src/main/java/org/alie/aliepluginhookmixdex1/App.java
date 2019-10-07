c6fac219211025ebbeafaadc0cc9f785feb87838
AlieYangpai <yangpai_beibei@163.com>
2019-10-07 22:25:03 +0800

    first commit

---
+++ app/src/main/java/org/alie/aliepluginhookmixdex1/App.java
@@ -0,0 +1,54 @@
+package org.alie.aliepluginhookmixdex1;
+
+import android.app.Application;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.os.Environment;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Alie on 2019/10/6.
+ * 类描述
+ * 版本
+ */
+public class App extends Application {
+    private HookUtil hookUtil;
+    private AssetManager assetManager;
+    private Resources newResource;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        hookUtil = new HookUtil();
+        hookUtil.injectPluginDex(this);
+
+
+        String apkPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/playchess-debug.apk";
+        try {
+            assetManager = AssetManager.class.newInstance();
+            Method addAssetPathMethod = assetManager.getClass().getDeclaredMethod("addAssetPath", String.class);
+            addAssetPathMethod.setAccessible(true);
+            addAssetPathMethod.invoke(assetManager, apkPath);
+//        手动实例化
+            Method ensureStringBlocks = AssetManager.class.getDeclaredMethod("ensureStringBlocks");
+            ensureStringBlocks.setAccessible(true);
+            ensureStringBlocks.invoke(assetManager);
+//            插件的StringBloac被实例化了
+            Resources supResource = getResources();
+            newResource = new Resources(assetManager, supResource.getDisplayMetrics(), supResource.getConfiguration());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public AssetManager getAssetManager() {
+        return assetManager==null?super.getAssets():assetManager;
+    }
+
+    @Override
+    public Resources getResources() {
+        return newResource==null?super.getResources():newResource;
+    }
+}
